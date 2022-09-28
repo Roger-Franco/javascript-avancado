c923eda9c004f43c05f74c9f5633479fe383705a
Roger-Franco <rogerfrancogalt@gmail.com>
2022-09-28 11:26:26 -0400

    Lança erro caso o desenvolvedor se esqueça de implementar o método _template

--- js/app/views/MensagemView.js
+++ js/app/views/MensagemView.js
@@ -4,7 +4,7 @@ class MensagemView extends View {
     super(elemento)
   }
 
-  _template(model) {
+  template(model) {
     // if (model.texto === '') return '<p></p>';
     // return `<p class="alert alert-info">${model.texto}</p>`
     /* Ou */
