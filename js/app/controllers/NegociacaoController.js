01a45306120c10285fcab58d31fd02fdd4cccc95
Roger-Franco <rogerfrancogalt@gmail.com>
2022-10-04 16:24:41 -0400

    Separa em um service a importação de negociações

--- js/app/controllers/NegociacaoController.js
+++ js/app/controllers/NegociacaoController.js
@@ -29,29 +29,16 @@ class NegociacaoController {
   }
 
   importaNegociacoes() {
-    let xhr = new XMLHttpRequest();
-    xhr.open('GET', 'negociacoes/semana')
-    xhr.onreadystatechange = () => {
-      /* 
-      0: requisição ainda não iniciada;
-      1: conexão com o servidor estabelecida;
-      2: requisição recebida;
-      3: processando requisição;
-      4: requisição consluída e a resposta está pronta.
-      */
-      if (xhr.readyState == 4) {
-        if (xhr.status == 200) {
-          JSON.parse(xhr.responseText)
-            .map(objeto => new Negociacao(new Date(objeto.data), objeto.quantidade, objeto.valor))
-            .forEach(negociacao => this._listaNegociacoes.adiciona(negociacao));
-          this._mensagem.texto = 'Negociações importadas com sucesso.';
-        } else {
-          console.log(xhr.responseText);
-          this._mensagem.texto = 'Não foi possível obter as negociações.';
-        }
+    let service = new NegociacaoService()
+
+    service.obterNegociacoesDaSemana((erro, negociacoes) => {
+      if (erro) {
+        this._mensagem.texto = erro;
+        return;
       }
-    }
-    xhr.send()
+      negociacoes.forEach(negociacao => this._listaNegociacoes.adiciona(negociacao));
+      this._mensagem.texto = 'Negociações importadas com sucesso';
+    })
   }
 
   apaga() {
